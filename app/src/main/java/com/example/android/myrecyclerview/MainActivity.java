894d714ac953dd2748fe6aa1f21df8daf1b30008
safrinauriya <safrina__auriya_28rpl@student.smktelkom-mlg.sch.id>
2020-09-15 21:40:00 +0700

    first commit

---
+++ app/src/main/java/com/example/android/myrecyclerview/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.myrecyclerview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_hero);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+    }
+}
