941e5d7b9960408d47e8fb14d13b2ddf0dbd4e22
safrinauriya <safrina__auriya_28rpl@student.smktelkom-mlg.sch.id>
2020-09-18 17:31:34 +0700

    fourth commit

--- app/src/main/java/com/example/android/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/android/myrecyclerview/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -32,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes.setAdapter(listHeroAdapter);
     }
 
+    private void showRecyclerGrid() {
+        rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -47,8 +54,10 @@ public class MainActivity extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
+                showRecyclerList();
                 break;
             case R.id.action_grid:
+                showRecyclerGrid();
                 break;
             case R.id.action_cardview:
                 break;
