577c6c46ea8816e413a80e0c5356de433f25b0ae
safrinauriya <safrina__auriya_28rpl@student.smktelkom-mlg.sch.id>
2020-09-22 11:27:01 +0700

    sixth commit

--- app/src/main/java/com/example/android/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/android/myrecyclerview/MainActivity.java
@@ -14,11 +14,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvHeroes;
     private ArrayList<Hero> list = new ArrayList<>();
+    private String title = "Mode List";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setActionBarTitle(title);
 
         rvHeroes = findViewById(R.id.rv_hero);
         rvHeroes.setHasFixedSize(true);
@@ -39,6 +41,19 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes.setAdapter(gridHeroAdapter);
     }
 
+    private void showRecyclerCardView() {
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewHeroAdapter);
+
+    }
+
+    private void setActionBarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -54,12 +69,16 @@ public class MainActivity extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
+                title = "Mode List";
                 showRecyclerList();
                 break;
             case R.id.action_grid:
                 showRecyclerGrid();
+                title = "ModeGrid";
                 break;
             case R.id.action_cardview:
+                showRecyclerCardView();
+                title = "Mode CardView";
                 break;
         }
     }
